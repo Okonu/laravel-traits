0660f53bcb6746095bda51e2944985d27e00b3da
Okonu <ianyakundi015@gmail.com>
2023-07-16 20:14:29 +0300

    learning traits

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::resource('students', StudentController::class);
+
+Route::resource('/', StudentController::class);
